d606bd9bb35c2e7b3586901f9a78c5365a115c07
cris <cristian192003@outlook.es>
2022-06-18 01:38:56 -0500

    Fondo transparente esperado

--- src/components/info/info.jsx
+++ src/components/info/info.jsx
@@ -4,7 +4,14 @@ import "./info.css";
 export function Info() {
   return (
     <div className="sec2">
-        <div className="trian2"></div>
+      <div className="trian2"></div>
+      <div class="background">
+        <div className="hijo">
+        <div className="linea_l"></div>
+        <div className="linea_l2"></div>
+        </div>
+      </div>
+      <div className="trian2"></div>
       <div className="centro">
         <div className="linea_l"></div>
         <div className="linea_l2"></div>
