15c74f61845f6fd2157d7c125ca183ba46e2c1c0
cris <cristian192003@outlook.es>
2022-06-28 18:52:11 -0500

    Version Pruebas

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://Cristiansb2003.github.io/PruebasHM/",
   plugins: [react()]
 })
